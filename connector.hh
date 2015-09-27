2bf340f1ededfed0ff2c2465ef11065bbbbbf559
Maxim Shishmarev <msgmaxim@gmail.com>
2015-09-28 01:03:21 +0800

    exposing Node constructor (and specification methods) to solvers

--- connector.hh
+++ connector.hh
@@ -1,61 +1,106 @@
 #ifndef CONNECTOR
 #define CONNECTOR
 
-#include <zmq.hpp>
+#include "message.pb.hh"
+#include "zmq.hpp"
 
 namespace message {
   class Node;
 }
 
-class Connector {
+namespace Profiling {
 
-private:
+  class Node;
 
-  const unsigned int port;
-  unsigned int _thread_id;
+  enum NodeStatus {
+    SOLVED = 0,
+    FAILED = 1,
+    BRANCH = 2,
+    SKIPPED = 6,
+    MERGING = 7
+  };
 
-  zmq::context_t context;
-  zmq::socket_t socket;
+  class Connector {
 
-  void sendOverSocket(message::Node& msg);
+  private:
+
+    const unsigned int port;
+    unsigned int _thread_id;
+
+    zmq::context_t context;
+    zmq::socket_t socket;
+
+    void sendOverSocket(const message::Node& msg);
 
 
-public:
+  public:
 
-  Connector(unsigned int port, unsigned int tid = 0);
+    Connector(unsigned int port, unsigned int tid = 0);
 
-  void connectToSocket();
+    /// connect to a socket via port specified in the construction (6565 by default)
+    void connect();
 
-  // sends START_SENDING message to Gist with model name
-  void restartGist(int restart_id, const std::string& file_path);
+    // sends START_SENDING message to the Profiler with a model name
+    void restart(int restart_id, const std::string& file_path);
 
-  void sendDoneSending();
+    void done();
 
-  void disconnectFromSocket();
+    /// disconnect from a socket
+    void disconnect();
 
-  void sendNode(int sid,
-                int pid,
-                int alt,
-                int kids,
-                int status,
-                const char* label,
-                unsigned int thread,
-                int restart = -1,
-                float domain = -1,
-                const std::string& nogood = "",
-                const std::string& info = "");
+    void sendNode(int sid,
+                  int pid,
+                  int alt,
+                  int kids,
+                  NodeStatus status,
+                  const char* label,
+                  unsigned int thread,
+                  int restart = -1,
+                  float domain = -1,
+                  const std::string& nogood = "",
+                  const std::string& info = "");
 
-  void sendNode(int sid,
-                int pid,
-                int alt,
-                int kids,
-                int status,
-                unsigned int thread,
-                int restart = -1,
-                float domain = -1,
-                const std::string& nogood = "",
-                const std::string& info = "");
+    void sendNode(int sid,
+                  int pid,
+                  int alt,
+                  int kids,
+                  NodeStatus status,
+                  unsigned int thread,
+                  int restart = -1,
+                  float domain = -1,
+                  const std::string& nogood = "",
+                  const std::string& info = "");
 
-};
+    void sendNode(const Profiling::Node& node);
+
+  };
+
+  class Node {
+
+  private:
+
+    message::Node _node;
+
+    const message::Node& get_node() const {
+      return _node;
+    }
+
+  public:
+
+    friend class Connector;
+
+    Node(int sid, int pid, int alt, int kids, NodeStatus status);
+
+    inline void set_label(const std::string& label) {
+      _node.set_label(label);
+    }
+
+    inline void set_thread_id(unsigned int thread_id) {
+      _node.set_thread_id(thread_id);
+    }
+
+  };
+
+}
 
 #endif
