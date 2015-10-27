29fc9688efbb0aa1d6fac567c0883c4fcf6f556f
Maxim Shishmarev <maxim.shishmarev@monash.edu>
2015-10-27 17:16:01 +1100

    sending correct restart_id

--- connector.cpp
+++ connector.cpp
@@ -120,7 +120,7 @@ namespace Profiling {
     dummy_node.set_type(message::Node::START);
 
     /// should it be restart_id instead?
-    dummy_node.set_restart_id(-1);
+    dummy_node.set_restart_id(restart_id);
 
     /// extract fzn file name
     std::string name(file_path);
