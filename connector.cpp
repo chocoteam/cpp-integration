5491168fdf4396a1ee98978f72060cc4404af2ce
Maxim Shishmarev <maxim.shishmarev@monash.edu>
2017-06-01 16:10:05 +1000

    OWN PROTOCOL

--- connector.cpp
+++ connector.cpp
@@ -1,19 +1,19 @@
 #include "connector.hh"
+
 #include <iostream>
 #include <ctime>
 #include <sstream>
 #include <cstdint>
-
-#include <cstdio>
+#include <vector>
 #include <cstring>
 
 #ifdef WIN32
 
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#pragma comment (lib, "Ws2_32.lib")
-#pragma comment (lib, "Mswsock.lib")
-#pragma comment (lib, "AdvApi32.lib")
+#pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "Mswsock.lib")
+#pragma comment(lib, "AdvApi32.lib")
 
 #else
 
@@ -27,227 +27,180 @@
 
 #endif
 
-template <typename T>
-std::string NumberToString ( T Number )
-{
-  std::ostringstream ss;
-  ss << Number;
-    return ss.str();
-}
+using std::vector;
+
+using cpprofiler::NodeStatus;
 
 namespace Profiling {
 
-  Node::Node(int sid, int pid, int alt, int kids, NodeStatus status, Connector& c) : _c(c) {
+Node::Node(int sid, int pid, int alt, int kids, NodeStatus status, Connector& c)
+    : _c(c), sid_(sid), pid_(pid), alt_(alt), kids_(kids), status_(status) {}
 
-    _node.set_type(message::Node::NODE);
+void Node::send() { this->_c.sendNode(*this); }
 
-    _node.set_sid(sid);
-    _node.set_pid(pid);
-    _node.set_alt(alt);
-    _node.set_kids(kids);
-    _node.set_status(static_cast<message::Node::NodeStatus>(status));
-    _node.set_thread_id(-1); /// -1 is default for thread id
-  }
+Connector::Connector(unsigned int port, unsigned int wid) : port(port), _connected(false) {}
 
-  void Node::send() {
-    this->_c.sendNode(*this);
-  }
+// From http://beej.us/guide/bgnet/output/html/multipage/advanced.html#sendall
+static int sendall(int s, const char* buf, int* len) {
+  int total = 0;         // how many bytes we've sent
+  int bytesleft = *len;  // how many we have left to send
+  int n;
 
-  Node::~Node() {
-
-  }
-
-}
-
-namespace Profiling {
-
-  // get sockaddr, IPv4 only!
-  void *get_in_addr(struct sockaddr *sa)
-  {
-    if (sa->sa_family == AF_INET) {
-      return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
-    return NULL;
-  }
-
-  Connector::Connector(unsigned int port, unsigned int tid)
-    : port(port), _thread_id(tid),
-      // socket(service),
-      _connected(false)
-  {}
-
-  // From http://beej.us/guide/bgnet/output/html/multipage/advanced.html#sendall
-  int sendall(int s, const char *buf, int *len)
-  {
-    int total = 0;        // how many bytes we've sent
-    int bytesleft = *len; // how many we have left to send
-    int n;
-        
-    while(total < *len) {
-      n = send(s, buf+total, bytesleft, 0);
-      if (n == -1) { break; }
-      total += n;
-      bytesleft -= n;
-    }
-        
-    *len = total; // return number actually sent here
-        
-    return n==-1?-1:0; // return -1 on failure, 0 on success
-  } 
-    
-  void Connector::sendOverSocket(const message::Node &msg) {
-    if (!_connected) return;
-    std::string msg_str;
-    msg.SerializeToString(&msg_str);
-
-    char *buf = new char[msg_str.size()];
-    memcpy(buf, msg_str.c_str(), msg_str.size());
-
-    sendRawMsg(buf, msg_str.size());
-  }
-
-  void Connector::sendRawMsg(const char* buf, int len) {
-    uint32_t bufSize = len;
-    int bufSizeLen = sizeof(uint32_t);
-    sendall(sockfd, reinterpret_cast<char*>(&bufSize), &bufSizeLen);
-    int bufSizeInt = bufSize;
-    sendall(sockfd, buf, &bufSizeInt);
-  }
-
-  void Connector::sendNode(int sid, int pid, int alt, int kids,
-                           NodeStatus status, const char* label, unsigned int thread, int restart,
-                           float domain, const std::string& nogood, int nogood_bld, bool uses_assumptions, const std::string& info) {
-    if (!_connected) return;
-
-    message::Node node;
-
-    node.set_type(message::Node::NODE);
-
-    node.set_sid(sid);
-    node.set_pid(pid);
-    node.set_alt(alt);
-    node.set_kids(kids);
-
-    node.set_status(static_cast<message::Node::NodeStatus>(status));
-    node.set_label(label);
-    node.set_thread_id(thread);
-    node.set_restart_id(restart);
-    node.set_domain_size(domain);
-    // node.set_solution(solution);
-    node.set_nogood(nogood);
-    node.set_nogood_bld(nogood_bld);
-    node.set_uses_assumptions(uses_assumptions);
-    node.set_info(info);
-
-    sendOverSocket(node);
-  }
-
-  Node Connector::createNode(int sid, int pid, int alt, int kids, NodeStatus status) {
-    return Node(sid, pid, alt, kids, status, *this);
-  }
-
-  void Connector::sendNode(const Profiling::Node& node) {
-    sendOverSocket(node.get_node());
-  }
-
-  void Connector::restart(const std::string& file_path, int restart_id, const std::string& variableList, int execution_id) {
-    // std::cerr << "restarting gist, restart_id: " << restart_id << "\n";
-
-    message::Node dummy_node;
-    dummy_node.set_type(message::Node::START);
-
-    /// should it be restart_id instead?
-    dummy_node.set_restart_id(restart_id);
-
-    /// extract fzn file name
-    std::string name(file_path);
-    int pos = name.find_last_of('/');
-    if (pos > 0) {
-      name = name.substr(pos + 1, name.length() - pos - 1);
-    }
-
-    dummy_node.set_label(name);
-
-    std::stringstream infoStream;
-    infoStream << "{";
-    if(execution_id != -1) {
-      infoStream << "\"execution_id\": " << execution_id;
-    }
-    if (variableList.size() > 0) {
-      if(execution_id != -1) infoStream << ", ";
-      infoStream << "\"variable_list\": \"" << variableList << "\"";
-    }
-    infoStream << "}";
-    dummy_node.set_info(infoStream.str());
-
-    sendOverSocket(dummy_node);
-  }
-
-  void Connector::connect() {
-    struct addrinfo hints, *servinfo, *p;
-    int rv;
-
-    #ifdef WIN32
-    // Initialise Winsock.
-    WSADATA wsaData;
-    int startupResult = WSAStartup(MAKEWORD(2,2), &wsaData);
-    if (startupResult != 0) {
-        printf("WSAStartup failed with error: %d\n", startupResult);
-    }
-    #endif
-
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-
-    if ((rv = getaddrinfo("localhost", std::to_string(port).c_str(), &hints, &servinfo)) != 0) {
-      std::cerr << "getaddrinfo: " << gai_strerror(rv) << "\n";
-      goto giveup;
-    }
-
-    // loop through all the results and connect to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-      if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                           p->ai_protocol)) == -1) {
-        // errno is set here, but we don't examine it.
-        continue;
-      }
-      
-      if (::connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-	#ifdef WIN32
-	closesocket(sockfd);
-	#else
-        close(sockfd);
-	#endif
-        // errno is set here, but we don't examine it.
-        continue;
-      }
-     
+  while (total < *len) {
+    n = send(s, buf + total, bytesleft, 0);
+    if (n == -1) {
       break;
     }
+    total += n;
+    bytesleft -= n;
+  }
 
-    // Connection failed; give up.
-    if (p == NULL) {
-      goto giveup;
+  *len = total;  // return number actually sent here
+
+  return n == -1 ? -1 : 0;  // return -1 on failure, 0 on success
+}
+
+void Connector::sendOverSocket() {
+  if (!_connected) return;
+
+  vector<char> buf = marshalling.serialize();
+
+  sendRawMsg(buf);
+}
+
+void Connector::sendRawMsg(const vector<char>& buf) {
+  uint32_t bufSize = buf.size();
+  int bufSizeLen = sizeof(uint32_t);
+  sendall(sockfd, reinterpret_cast<char*>(&bufSize), &bufSizeLen);
+  int bufSizeInt = bufSize;
+  sendall(sockfd, reinterpret_cast<const char*>(buf.data()), &bufSizeInt);
+}
+
+
+Node Connector::createNode(int sid, int pid, int alt, int kids,
+                           NodeStatus status) {
+  return Node(sid, pid, alt, kids, status, *this);
+}
+
+void Connector::sendNode(const Profiling::Node& node) {
+
+  if (!_connected) return;
+
+  auto& msg = marshalling.makeNode(node.sid(), node.pid(), node.alt(), node.kids(), node.status());
+
+  if (node.label().valid()) msg.set_label(node.label().value());
+
+  if (node.nogood().valid()) msg.set_nogood(node.nogood().value());
+
+  if (node.info().valid()) msg.set_info(node.info().value());
+
+  if (node.solution().valid()) msg.set_solution(node.solution().value());
+
+  if (node.tid().valid()) msg.set_tid(node.tid().value());
+
+  if (node.rid().valid()) msg.set_rid(node.rid().value());
+
+  sendOverSocket();
+}
+
+void Connector::restart(const std::string& file_path, int rid,
+                        const std::string& variableList, int ex_id) {
+  /// extract fzn file name
+  std::string label(file_path);
+  {
+    int pos = label.find_last_of('/');
+    if (pos > 0) {
+      label = label.substr(pos + 1, label.length() - pos - 1);
     }
-    
-    freeaddrinfo(servinfo); // all done with this structure
-
-    _connected = true;
-
-    return;
-  giveup:
-    _connected = false;
-    return;
   }
 
-  void Connector::done() {
-    message::Node dummy_node;
-    dummy_node.set_type(message::Node::DONE);
-    sendOverSocket(dummy_node);
+  std::string info{""};
+  {
+    std::stringstream ss;
+    ss << "{";
+    if (ex_id != -1) {
+      ss << "\"execution_id\": " << ex_id;
+    }
+    if (variableList.size() > 0) {
+      if (ex_id != -1) ss << ", ";
+      ss << "\"variable_list\": \"" << variableList << "\"";
+    }
+    ss << "}";
+    info = ss.str();
   }
 
-  void Connector::disconnect() {
+  marshalling.makeStart(rid, label, info);
+  sendOverSocket();
+}
+
+void Connector::connect() {
+  struct addrinfo hints, *servinfo, *p;
+  int rv;
+
+#ifdef WIN32
+  // Initialise Winsock.
+  WSADATA wsaData;
+  int startupResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+  if (startupResult != 0) {
+    printf("WSAStartup failed with error: %d\n", startupResult);
   }
+#endif
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+
+  if ((rv = getaddrinfo("localhost", std::to_string(port).c_str(), &hints,
+                        &servinfo)) != 0) {
+    std::cerr << "getaddrinfo: " << gai_strerror(rv) << "\n";
+    goto giveup;
+  }
+
+  // loop through all the results and connect to the first we can
+  for (p = servinfo; p != NULL; p = p->ai_next) {
+    if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+      // errno is set here, but we don't examine it.
+      continue;
+    }
+
+    if (::connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+#ifdef WIN32
+      closesocket(sockfd);
+#else
+      close(sockfd);
+#endif
+      // errno is set here, but we don't examine it.
+      continue;
+    }
+
+    break;
+  }
+
+  // Connection failed; give up.
+  if (p == NULL) {
+    goto giveup;
+  }
+
+  freeaddrinfo(servinfo);  // all done with this structure
+
+  _connected = true;
+
+  return;
+giveup:
+  _connected = false;
+  return;
+}
+
+void Connector::done() {
+  marshalling.makeDone();
+  sendOverSocket();
+}
+
+void Connector::disconnect() {}
+
+std::ostream& operator<<(std::ostream& os, Node& n) {
+  return os << "print node here";
+}
+
 
 }
--- connector.hh
+++ connector.hh
@@ -3,191 +3,151 @@
 #ifndef CONNECTOR
 #define CONNECTOR
 
-#include "message.pb.hh"
-// #include "nn.hpp"
-// #include <nanomsg/pipeline.h>
-// #include <nanomsg/tcp.h>
+#include "message.hh"
+#include <ostream>
 
-// #include <boost/asio.hpp>
-
-// using namespace boost::asio;
-// using namespace boost::asio::ip;
-
-#include <iostream>
-
-namespace message {
-  class Node;
-}
+class vector;
 
 namespace Profiling {
 
-  class Node;
+  using NodeStatus = cpprofiler::NodeStatus;
 
-  enum NodeStatus {
-    SOLVED = 0,
-    FAILED = 1,
-    BRANCH = 2,
-    SKIPPED = 6,
-    MERGING = 7
-  };
+class Node;
 
-  class Connector {
+template<typename T>
+class Option {
 
-  private:
+  bool is_valid{false};
+  T value_;
 
-    const unsigned int port;
-    unsigned int _thread_id;
+public:
 
-    // nn::socket nanosocket;
-      // int socketfd;
-      // io_service service;
-      // tcp::socket socket;
-      int sockfd;
-      bool _connected;
-    // int endpoint;
+  Option() = default;
+  Option(T&& t): is_valid{true}, value_{t} {}
+  Option(const Option& other) = default;
+  Option& operator=(Option& other) = default;
 
-    void sendOverSocket(const message::Node& msg);
+  const T& value() const { return value_; }
+  bool valid() const { return is_valid; }
+  void set(const T& v) { is_valid = true; value_ = v; }
 
-  public:
-    void sendRawMsg(const char* buf, int len);
+};
 
-    Connector(unsigned int port, unsigned int tid = 0);
+class Connector {
+ private:
 
-      bool connected() { return _connected; }
 
-    /// connect to a socket via port specified in the construction (6565 by default)
-    void connect();
+  MessageMarshalling marshalling;
 
-    // sends START_SENDING message to the Profiler with a model name
-    void restart(const std::string& file_path = "", int restart_id = -1, const std::string& variableList = "", int execution_id=-1);
+  const unsigned int port;
 
-    void done();
+  int sockfd;
+  bool _connected;
 
-    /// disconnect from a socket
-    void disconnect();
+  void sendOverSocket();
 
-    void sendNode(int sid,
-                  int pid,
-                  int alt,
-                  int kids,
-                  NodeStatus status,
-                  const char* label,
-                  unsigned int thread = -1,
-                  int restart = -1,
-                  float domain = -1,
-                  const std::string& nogood = "",
-                  int nogood_bld = -1,
-                  bool uses_assumptions = false,
-                  const std::string& info = "");
+ public:
+  void sendRawMsg(const std::vector<char>& buf);
 
-    void sendNode(const Profiling::Node& node);
+  Connector(unsigned int port, unsigned int wid = -1);
 
-    Node createNode(int sid, int pid, int alt, int kids, NodeStatus status);
+  bool connected() { return _connected; }
 
-  };
+  /// connect to a socket via port specified in the construction (6565 by
+  /// default)
+  void connect();
 
-  class Node {
+  // sends START_SENDING message to the Profiler with a model name
+  void restart(const std::string& file_path = "", int restart_id = -1,
+               const std::string& variableList = "", int execution_id = -1);
 
-  private:
+  void done();
 
-    message::Node _node;
+  /// disconnect from a socket
+  void disconnect();
 
-    const message::Node& get_node() const {
-      return _node;
-    }
+  void sendNode(const Profiling::Node& node);
 
-    Node(const Node& node); /// no copying allowed
+  Node createNode(int sid, int pid, int alt, int kids, NodeStatus status);
+};
 
-    Connector& _c;
+std::ostream& operator<<(std::ostream& os, Node& n);
 
-  public:
+class Node {
 
-    friend class Connector;
+  using NodeStatus = cpprofiler::NodeStatus;
 
-    Node(int sid, int pid, int alt, int kids, NodeStatus status, Connector& c);
+  Connector& _c;
 
-    ~Node();
+  int sid_;
+  int pid_;
+  int alt_;
+  int kids_;
 
-    void send();
+  NodeStatus status_;
 
-    inline Node& set_label(const std::string& label) {
-      _node.set_label(label);
-      return *this;
-    }
+  Option<std::string> label_;
+  Option<std::string> nogood_;
+  Option<std::string> info_;
+  Option<std::string> solution_;
 
-    inline Node& set_thread_id(unsigned int thread_id) {
-      _node.set_thread_id(thread_id);
-      return *this;
-    }
+  Option<int> tid_;
+  Option<int> rid_;
 
-    inline Node& set_restart_id(int restart_id) {
-      _node.set_restart_id(restart_id);
-      return *this;
-    }
+ public:
 
-    inline Node& set_domain_size(float domain_size) {
-      _node.set_domain_size(domain_size);
-      return *this;
-    }
+  Node(int sid, int pid, int alt, int kids, NodeStatus status, Connector& c);
 
-    inline Node& set_nogood(const std::string& nogood) {
-      _node.set_nogood(nogood);
-      return *this;
-    }
+  Node& set_thread_id(int tid) {
+    tid_.set(tid);
+    return *this;
+  }
 
-    inline Node& set_nogood_bld(int nogood_bld) {
-      _node.set_nogood_bld(nogood_bld);
-      return *this;
-    }
+  Node& set_restart_id(int rid) {
+    rid_.set(rid);
+    return *this;
+  }
 
-    inline Node& set_uses_assumptions(bool uses_assumptions) {
-      _node.set_uses_assumptions(uses_assumptions);
-      return *this;
-    }
+  const Option<std::string>& label() const { return label_; }
 
-    inline Node& set_backjump_distance(int backjump_distance) {
-      _node.set_backjump_distance(backjump_distance);
-      return *this;
-    }
+  Node& set_label(const std::string& label) {
+    label_.set(label);
+    return *this;
+  }
 
-    inline Node& set_decision_level(int decision_level) {
-      _node.set_decision_level(decision_level);
-      return *this;
-    }
+  const Option<std::string>& nogood() const { return nogood_; }
 
-    inline Node& set_info(const std::string& nogood) {
-      _node.set_info(nogood);
-      return *this;
-    }
+  Node& set_nogood(const std::string& nogood) {
+    nogood_.set(nogood);
+    return *this;
+  }
 
-    inline Node& set_solution(const std::string& solution) {
-      _node.set_solution(solution);
-      return *this;
-    }
+  const Option<std::string>& info() const { return info_; }
 
-    inline Node& set_time(unsigned long long time) {
-      _node.set_time(time);
-      return *this;
-    }
+  Node& set_info(const std::string& info) {
+    info_.set(info);
+    return *this;
+  }
 
-    void print(std::ostream& out) {
-      out << _node.type() << ","
-          << _node.sid() << ","
-          << _node.restart_id() << ","
-          << _node.pid() << ","
-          << _node.alt() << ","
-          << _node.kids() << ","
-          << _node.status() << ","
-          << _node.time() << ","
-          << _node.label() << ","
-          << _node.nogood() << ","
-          << _node.nogood_bld() << ","
-          << _node.uses_assumptions() << ","
-          << _node.backjump_distance() << ","
-          << _node.decision_level() << ","
-          << _node.info() << "\n";
-    }
-  };
+  const Option<std::string>& solution() const { return solution_; }
+
+  Node& set_solution(const std::string& solution) {
+    solution_.set(solution);
+    return *this;
+  }
+
+  int sid() const { return sid_; }
+  int pid() const { return pid_; }
+  int alt() const { return alt_; }
+  int kids() const { return kids_; }
+
+  const NodeStatus status() const { return status_; }
+
+  Option<int> tid() const { return tid_; }
+  Option<int> rid() const { return rid_; }
+
+  void send();
+};
 }
 
 #endif
